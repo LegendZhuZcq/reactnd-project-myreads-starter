27e24f2fd937eb25cffe78c1d47a90f1f59fe9ae
LegendZhuZcq <zhu.legend@gmail.com>
2017-08-07 08:13:41 -0700

    Finished bookshelf

---
+++ src/Book.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Book extends Component{
+  static propTypes={
+    book:PropTypes.object.isRequired,
+    updateShelf:PropTypes.func
+  }
+
+  state={
+    currentShelf:this.props.book.shelf,
+    book:this.props.book
+  }
+
+  updateShelf =(e)=>{
+    this.props.updateShelf(this.props.book, e.target.value)
+    this.setState({currentShelf:e.target.value})
+  }
+
+  render(){
+        const{book}=this.props
+        return(
+          <li key={book.id}>
+                         <div className="book">
+                           <div className="book-top">
+                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                               <div className="book-shelf-changer">
+                                   <select
+                                       value={this.state.currentShelf}
+                                       onChange={this.updateShelf}
+                                   >
+                                       <option value="move" disabled>Move to...</option>
+                                       <option value="currentlyReading">Currently Reading</option>
+                                       <option value="wantToRead">Want to Read</option>
+                                       <option value="read">Read</option>
+                                       <option value="none">None</option>
+                                   </select>
+                               </div>
+                           </div>
+                           <div className="book-title">{book.title}</div>
+                           <div className="book-authors">{book.authors}</div>
+                         </div>
+                       </li>
+
+        )
+  }
+}
+
+export default Book
---
+++ src/BookShelf.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import Book from './Book'
+
+
+class BookShelf extends Component{
+  static propTypes={
+    books:PropTypes.array.isRequired,
+    shelf:PropTypes.string,
+    title:PropTypes.string.isRequired,
+    onUpdate:PropTypes.func
+  }
+
+
+ render(){
+
+     const{books, shelf,title, onUpdate}=this.props
+     const match = new RegExp(shelf)
+     let showingBooks = books.filter((book)=>match.test(book.shelf))
+     return(
+             <div className="bookshelf">
+               <h2 className="bookshelf-title">{title}</h2>
+                 <div className="bookshelf-books">
+                   <ol className="books-grid">
+                     {showingBooks.map((book)=>(
+                       <Book book={book} updateShelf={onUpdate}/>
+                     ))}
+               </ol>
+             </div>
+           </div>
+   )
+ }
+}
+
+export default BookShelf
